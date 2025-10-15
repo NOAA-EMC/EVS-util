1f916f9c8ea362c815331cc9f5352ef626cebef9
Alicia Bentley <alicia.bentley@noaa.gov>
2025-10-15 19:50:51 +0000

    Updating WAFS stats filename with ${STEP} update

--- drivers/drive_jevs_wafs_stats.sh
+++ drivers/drive_jevs_wafs_stats.sh
@@ -19,4 +19,4 @@ cd /lfs/h2/emc/ptmp/${USER}/output
 module reset
 
 drivers_dir=${HOMEevs}/dev/drivers/scripts/${STEP}/${COMPONENT}
-qsub ${drivers_dir}/jevs_wafs_atmos_stats.sh
+qsub ${drivers_dir}/jevs_stats_wafs_atmos.sh
