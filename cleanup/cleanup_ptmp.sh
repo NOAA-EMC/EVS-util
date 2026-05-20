b195de01b106fed961ebbe1174a233fbcbc16d4f
Mallory Row <mallory.row@noaa.gov>
2026-05-20 11:58:56 +0000

    Remove nfcens and some narre code

--- cleanup/cleanup_ptmp.sh
+++ cleanup/cleanup_ptmp.sh
@@ -24,8 +24,6 @@ rm -f -r /lfs/h2/emc/ptmp/emc.vpppg/evs/v2.0/plots/global_det/*${VDATE_RM}*
 rm -f -r /lfs/h2/emc/ptmp/emc.vpppg/evs/v2.0/plots/global_ens/*${VDATE_RM}*
 rm -f -r /lfs/h2/emc/ptmp/emc.vpppg/evs/v2.0/plots/glwu/*${VDATE_RM}*
 rm -f -r /lfs/h2/emc/ptmp/emc.vpppg/evs/v2.0/plots/mesoscale/*${VDATE_RM}*
-rm -f -r /lfs/h2/emc/ptmp/emc.vpppg/evs/v2.0/plots/narre/*${VDATE_RM}*
-rm -f -r /lfs/h2/emc/ptmp/emc.vpppg/evs/v2.0/plots/nfcens/*${VDATE_RM}*
 rm -f -r /lfs/h2/emc/ptmp/emc.vpppg/evs/v2.0/plots/nwps/*${VDATE_RM}*
 rm -f -r /lfs/h2/emc/ptmp/emc.vpppg/evs/v2.0/plots/rtofs/*${VDATE_RM}*
 rm -f -r /lfs/h2/emc/ptmp/emc.vpppg/evs/v2.0/plots/subseasonal/*${VDATE_RM}*
