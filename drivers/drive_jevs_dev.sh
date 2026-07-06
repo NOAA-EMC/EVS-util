6fe26d810be145bf7eeaed7a90927e52870dcec3
Mallory Row <mallory.row@noaa.gov>
2026-07-06 18:54:35 +0000

    Update driver and cron for rtofs, nwps, and glwu

--- drivers/drive_jevs_dev.sh
+++ drivers/drive_jevs_dev.sh
@@ -9,7 +9,7 @@ HOMEevs=/lfs/h2/emc/vpppg/noscrub/${USER}/EVS
 # If argument is passed via the environment, use it. Otherwise, use its default.
 STEP="${STEP:-"step"}"
 COMPONENT="${COMPONENT:-"component"}"
-JOB="${JOB:-"job"}"
+JOB="${JOB:-"null"}"
 VHR="${VHR:-"null"}"
 
 # 2. Parse command-line arguments (Overrides environment variables)
@@ -46,10 +46,6 @@ if [ ${COMPONENT} = "component" ]; then
     echo "ERROR: Did not pass component= or set COMPONENT in environment"
     exit 1
 fi
-if [ ${JOB} = "job" ]; then
-    echo "ERROR: Did not pass job= or set JOB in environment"
-    exit 1
-fi
 
 # 3. Submit job
 mkdir -p /lfs/h2/emc/ptmp/${USER}/output
@@ -58,7 +54,13 @@ cd /lfs/h2/emc/ptmp/${USER}/output
 module reset
 
 drivers_dir=${HOMEevs}/dev/drivers/scripts/${STEP}/${COMPONENT}
-if [[ "$JOB" == *"rrfsmem"* ]]; then
+if [ "$JOB" == "null" ]; then
+    if [ "$VHR" == "null" ]; then
+        qsub ${drivers_dir}/jevs_${STEP}_${COMPONENT}.sh
+    else
+        qsub -v vhr=$VHR ${drivers_dir}/jevs_${STEP}_${COMPONENT}.sh
+    fi
+elif [[ "$JOB" == *"rrfsmem"* ]]; then
     for mem in {1..5}; do
         qsub -v vhr=$vhr,mem=$mem ${drivers_dir}/jevs_${STEP}_${COMPONENT}_${JOB}.sh
     done
